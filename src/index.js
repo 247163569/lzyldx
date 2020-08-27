8aec1878ebe81267b29a2f966d1101dcd967a9dd
lizhaoyang <247163569@qq.com>
2020-08-27 17:15:29 +0800

    modify

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import Layout from './pages/layout/index';
 import './index.css';
 
 const AppDom = (
   <div style={{ height: '100%', width: '100%' }}>
-    <BrowserRouter>
+    <HashRouter>
       <Layout/>
-    </BrowserRouter>
+    </HashRouter>
   </div>
 )
 
